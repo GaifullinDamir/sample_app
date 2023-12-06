e8a9cf497a2f6c9e0bc880777258e964d4010109
GaifullinDamir <gayfullindr@gmail.com>
2023-12-06 15:08:11 +0300

    <=9.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Обрабатывает успешное обновление
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
@@ -26,3 +39,4 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
+
